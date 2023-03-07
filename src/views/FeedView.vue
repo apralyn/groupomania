c5b17e9b544927d061cca6791969958fa22d3dd9
apralyn eribal <aeribal@wgu.edu>
2023-03-06 22:55:04 -0800

    added dummy post on FeedPreview.

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,23 +1,48 @@
 <template>
   <div class="feed-container">
     <img class="feed-logo" src="../assets/icon-above-font.png" alt="Groupomania-logo">
-    <nav class="feed-nav">
-      <div class="Home">Home</div>
+    <div class="feed-nav">
       <div class="profile">Profile</div>
       <div class="add-post">Create a post</div>
-    </nav>
-    <div class="posts-cards-container">
-      <div class="post-card">1</div>
-      <div class="post-card">2</div>
-      <div class="post-card">3</div>
-      <div class="post-card">4</div>
-      <div class="post-card">5</div>
-      <div class="post-card">6</div>
-      <div class="post-card">7</div>
-      <div class="post-card">8</div>
     </div>
-    <div class="bottom-nav">
-
+    <div class="posts-cards-container">
+      <div class="post-card post-preview">
+        <div class="post-thumb">thumb</div>
+        <div class="post-description">
+          <div class="post-title">title</div>
+          <div class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. dolor sit amet consectetur
+            adipisicing elit.</div>
+        </div>
+        <div class="post-arrow-access">arrow</div>
+      </div>
+      <div class="post-card post-preview">
+        <div class="post-thumb">thumb</div>
+        <div class="post-description">
+          <div class="post-title">title</div>
+          <div class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. dolor sit amet consectetur
+            adipisicing elit.</div>
+        </div>
+        <div class="post-arrow-access">arrow</div>
+      </div><div class="post-card post-preview">
+        <div class="post-thumb">thumb</div>
+        <div class="post-description">
+          <div class="post-title">title</div>
+          <div class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. dolor sit amet consectetur
+            adipisicing elit.</div>
+        </div>
+        <div class="post-arrow-access">arrow</div>
+      </div>
+    </div>
+    <div class="next-page"> -- next -- </div>
+    <div class="bottom-nav-container">
+      <div class="bottom-nav-left">
+        <div>Links</div>
+        <div>Links</div>
+      </div>
+      <div class="bottom-nav-right">
+        <div>Links</div>
+        <div>Links</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,9 +50,12 @@
 </script>
 <style scoped>
 .feed-container {
-  width: 100vh;
-  margin: auto;
-  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  height: 100vh;
 }
 
 .feed-logo {
@@ -39,24 +67,68 @@
 .feed-nav {
   display: flex;
   justify-content: space-between;
-  padding: 10px 50px;
-  ;
-  margin: 20px;
+  width: 500px;
+  padding: 5px;
 }
 
 .posts-cards-container {
   width: 90vh;
-  height: 500px;
   margin: auto;
-  padding: 20px;
+  padding: 10px 0;
 
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: space-around;
 }
 
 .post-card {
-  width: 200px;
-  height: 225px;
+  width: 500px;
+  height: 80px;
+  margin: 3px;
+  text-align: start;
   border: 1px solid red;
+  list-style-type: none;
+}
+
+.next-page {
+  width: 500px;
+  height: 50px;
+  margin-top: 0;
+}
+
+.bottom-nav-container {
+  border: 1px solid yellow;
+  display: flex;
+  justify-content: space-between;
+  width: 500px;
+  height: 50px;
+  margin-top: 5px;
+}
+
+.post-preview {
+  display: flex;
+  padding: 3px;
+}
+
+.post-thumb {
+  width: 100px;
+  text-align: center;
+  border: 1px solid blue;
+  margin: 0;
+}
+
+.post-description {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px;
+  margin: 0 5px;
+}
+
+.post-arrow-access {
+  border: 1px solid blue;
+  padding-top: 20px;
+}
+
+.post-content {
+  font-size: 12px;
 }</style>
