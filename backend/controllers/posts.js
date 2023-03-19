8c6cc9c7e51520117e61e675c445590943753238
apralyn eribal <aeribal@wgu.edu>
2023-03-18 22:05:31 -0700

    addpost controller is working

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 //all postings (getAll)
 exports.getAllPosts = (req, res, next) => {
   Post.findAll()
-  .then((post) => {
-    res.status(200).json(post);
-  })
-  .catch((error) => {
-    res.status(500).json({
-      error: error.message,
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error.message,
+      });
     });
-  });
 };
 
 //view one post (viewPost)
@@ -29,8 +29,31 @@ exports.viewPost = (req, res, next) => {
 };
 
 //create a new one (addPost)
+exports.addPost = (req, res, next) => {
+  //const imageUrl = req.protocol + "://" + req.get("host");
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    //imageUrl: imageUrl + "/images/" + req.file.filename,
+    likes: 0,
+    usersLiked: [],
+    userId: req.body.userId,
+  });
+  post
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Post successfully added!",
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error,
+      });
+    });
+};
 
 // edit a post (modifyPost)
 
 //delete a post (deletePost)
-
