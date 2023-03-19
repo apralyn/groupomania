6ffe70e6c273145e6c46562e21c4d84f5bfee08b
apralyn eribal <aeribal@wgu.edu>
2023-03-18 21:35:01 -0700

    viewPost controller is working

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,6 +1,7 @@
 const { Post } = require("../models");
 const fs = require("fs");
 
+//all postings (getAll)
 exports.getAllPosts = (req, res, next) => {
   Post.findAll()
   .then((post) => {
@@ -12,3 +13,24 @@ exports.getAllPosts = (req, res, next) => {
     });
   });
 };
+
+//view one post (viewPost)
+exports.viewPost = (req, res, next) => {
+  Post.findOne({ where: { id: req.params.id } })
+    .then((post) => {
+      res.status(200).json(post);
+      console.log("Hello Cat");
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: error.message,
+      });
+    });
+};
+
+//create a new one (addPost)
+
+// edit a post (modifyPost)
+
+//delete a post (deletePost)
+
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const postsCtrl = require("../controllers/posts");
 
 router.get("/feed", postsCtrl.getAllPosts);
+router.get("/viewpost/:id", postsCtrl.viewPost);
+// router.post("/addpost", postsCtrl.addPost);
+// router.put("/:id", postsCtrl.modifyPost);
+// router.delete("/:id", postsCtrl.deletePost);
 
 module.exports = router;
