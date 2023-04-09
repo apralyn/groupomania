831512c851c601bb33f4300aa31ec7941cbaee8a
apralyn eribal <aeribal@wgu.edu>
2023-04-09 05:32:26 -0700

    comment out non-important

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,8 +7,8 @@ const postsCtrl = require("../controllers/posts");
 router.post("/addpost", multer, postsCtrl.addPost);
 router.get("/feed", postsCtrl.getAllPosts);
 router.get("/viewpost/:id", postsCtrl.viewPost);
-router.put("/mod/:id", multer, postsCtrl.modifyPost);
-router.post("/like/:id", postsCtrl.readPost);
-router.delete("/delete/:id", postsCtrl.deletePost);
+// router.put("/mod/:id", multer, postsCtrl.modifyPost);
+router.post("/read/:id", postsCtrl.readPost);
+// router.delete("/delete/:id", postsCtrl.deletePost);
 
 module.exports = router;
