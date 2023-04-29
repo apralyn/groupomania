d71b4c335c0474a1e8cb7ea751d238d218aabed9
apralyn eribal <aeribal@wgu.edu>
2023-04-29 15:07:31 -0700

    created tables

---
+++ backend/db.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+
+const app = express();
+
+// Create a pool to handle database connections
+const pool = new Pool({
+  user: 'newuser',
+  host: 'localhost',
+  database: 'groupomania',
+  password: 'password',
+  port: 5432,
+});
+
+// Create a table for user model
+const createUserTable = () => {
+  pool.query(
+    CREATE TABLE IF NOT EXISTS users (
+      userId SERIAL PRIMARY KEY,
+      username VARCHAR(50) UNIQUE NOT NULL,
+      initials VARCHAR(2)  NOT NULL,
+      password VARCHAR(50) NOT NULL,
+      email VARCHAR(50) UNIQUE NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    )
+  )
+    .then(() => console.log('User table created'))
+    .catch((err) => console.error('Error creating user table', err));
+};
+
+// Create a table for post model
+const createPostTable = () => {
+  pool.query(
+    CREATE TABLE IF NOT EXISTS posts (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(50) NOT NULL,
+      description TEXT NOT NULL,
+      imageURL VARCHAR(50) NOT NULL,
+      usersRead INTEGER[] NOT NULL,
+      userId INTEGER REFERENCES users(id) NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    )
+  )
+    .then(() => console.log('Post table created'))
+    .catch((err) => console.error('Error creating post table', err));
+};
+
+// Call the table creation functions
+createUserTable();
+createPostTable();
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
